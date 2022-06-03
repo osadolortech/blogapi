c38346daacc6474a9027746a7950b47895dbe3cb
osadolortech <brightosadolor8@gmail.com>
2022-06-03 18:20:01 +0100

    #1 CREATED THE POST METHOD

--- routes/blog_routes.py
+++ routes/blog_routes.py
@@ -12,3 +12,10 @@ blog_route = APIRouter()
 async def get_all_post():
     blog = blogs_schemas(collection_post.find())
     return {"status": "ok", "All_post": blog}
+
+@blog_route.post("/")
+async def blog_post(blog: Blog):
+    _id = collection_post.insert_one(dict(blog))
+    blog = blogs_schemas(collection_post.find({"_id": _id.inserted_id}))
+    return {"status": "ok", "post_blog": blog}
+    
