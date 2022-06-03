a9e0ee274771a7846b689cf93e5fd5fa6d5b4a6e
osadolortech <brightosadolor8@gmail.com>
2022-06-03 16:09:35 +0100

    #1 fixed env variable problems

--- config/database.py
+++ config/database.py
@@ -2,10 +2,9 @@ from http import client
 from os import getenv
 from pymongo import MongoClient
 
-username = getenv("DB_USERNAME")
-password = getenv("DB_BASE")
+URL = getenv("URL")
 
-client = MongoClient("mongodb+srv://username:password@cluster0.ve2nnja.mongodb.net/mainblog?retryWrites=true&w=majority")
+client = MongoClient(URL)
 
 db = client.blog_application
 
