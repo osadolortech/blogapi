fcdb694b80f6502f074ed0286750696deeb67b54
osadolortech <brightosadolor8@gmail.com>
2022-06-06 18:54:13 +0100

    #1 project completed,updated modules,route,schemas

--- models/blog_models.py
+++ models/blog_models.py
@@ -1,4 +1,20 @@
 from pydantic import BaseModel
+from bson.objectid import  ObjectId
+
+from bson.errors import InvalidId
+
+class ObjectIdStr(str):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+    
+    @classmethod
+    def validate(cls, v):
+        try:
+            ObjectId(v)
+        except InvalidId:
+            raise ValueError("Not a VALID OBJECTID")
+        return str(v)
 
 
 class Blog(BaseModel):
@@ -6,5 +22,9 @@ class Blog(BaseModel):
     title: str
     content: str
 
+
 class Comment(BaseModel):
     content: str  
+    post: ObjectIdStr
+
+
